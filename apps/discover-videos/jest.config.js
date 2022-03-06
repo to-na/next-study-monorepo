6c47796056903073a0edd33a5c6547962301c21b
to-na <tonanatz@gmail.com>
2022-03-06 20:13:28 +0900

    lesson 188

---
+++ apps/discover-videos/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'discover-videos',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/discover-videos',
+};
