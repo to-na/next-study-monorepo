258028449d45ec520434897ab9dfc1e702cffed9
to-na <tonanatz@gmail.com>
2022-03-06 17:16:56 +0900

    vercel-build-zero-sintall

--- tools/ignore-vercel-build.sh
+++ tools/ignore-vercel-build.sh
@@ -2,16 +2,11 @@
 # Name of the app to check. Change this to your application name!
 APP=discover-coffee-stores
 
-# Determine version of Nx installed
-NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
-TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
-
 # Install @nrwl/workspace in order to run the affected command
-npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline
-npm install -D typescript@$TS_VERSION --prefer-offline
+yarn install
 
 # Run the affected command, comparing latest commit to the one before that
-npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+yarn run nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 
 # Store result of the previous command (grep)
 IS_AFFECTED=$?
